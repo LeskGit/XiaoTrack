ccca71d61553bbf84e7505ca530637820bb466f4
Axel Meunier <143808857+LeskGit@users.noreply.github.com>
2026-04-13 22:45:24 +0200

    update Mainlayout grid

--- apps/web/src/layout/MainLayout.tsx
+++ apps/web/src/layout/MainLayout.tsx
@@ -6,9 +6,13 @@ export default function MainLayout() {
     const date = new Date();
 
     return (
-        <div className='grid'>
-            <MainHeader categorieName="Tableau de bord" date={date} />
-            <MainSidebar></MainSidebar>
+        <div className='grid md:grid-cols-[20%_1fr]'>
+            <div className='col-span-2'>
+                <MainHeader categorieName="Tableau de bord" date={date} />
+            </div>
+            <div className=''>
+                <MainSidebar></MainSidebar>
+            </div>
         </div>
     )
 }
