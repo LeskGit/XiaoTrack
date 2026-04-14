f1b11046e6be06acf2c314f93f78e607e1771261
Axel Meunier <143808857+LeskGit@users.noreply.github.com>
2026-04-14 17:40:03 +0200

    Upadte layout Main

--- apps/web/src/layout/MainLayout.tsx
+++ apps/web/src/layout/MainLayout.tsx
@@ -6,8 +6,8 @@ export default function MainLayout() {
     const date = new Date();
 
     return (
-        <div className='grid md:grid-cols-[20%_1fr]'>
-            <div className='col-span-2'>
+        <div className='grid md:grid-cols-[16rem_1fr]'>
+            <div className='col-span-full'>
                 <MainHeader categorieName="Tableau de bord" date={date} />
             </div>
             <div className=''>
